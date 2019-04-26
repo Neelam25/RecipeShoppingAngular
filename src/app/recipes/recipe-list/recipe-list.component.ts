3d121fa0d3e2e672ea87f1ec5f0171eb88e88045
Neelam <neelam.chaurasia@mobilelive.ca>
2019-04-26 13:26:49 -0400

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Description Recipe', 'http://www.pngall.com/wp-content/uploads/2016/03/Food.png '),
+    new Recipe('Test Recipe', 'Test Description Recipe', 'http://www.pngall.com/wp-content/uploads/2016/03/Food.png ')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
